34cd248df2419f0da24d605c2502e98c7dca3495
fe-lam-dinh <dinhtronglam172000@gmail.com>
2023-04-18 11:50:20 +0700

    FEAT: update 18/4

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const compression = require("compression");
 const app = express();
 
+console.log(`Process`, process.env);
 // init middlewares
 app.use(morgan("dev"));
 app.use(helmet());
@@ -11,8 +13,9 @@ app.use(compression());
 
 // init db
 require("./dbs/init.mongodb");
-const { checkOverload } = require("./helpers/check.connect");
-checkOverload();
+// const { checkOverload } = require("./helpers/check.connect");
+// checkOverload();
+
 // init routes
 app.get("/", (req, res, next) => {
   const strCompress = "Trong Lam co len";
--- src/dbs/init.mongodb.js
+++ src/dbs/init.mongodb.js
@@ -1,9 +1,12 @@
 "use strict";
 const mongoose = require("mongoose");
+const {
+  db: { host, port, name },
+} = require("../configs/config.mongodb");
 const { countConnect } = require("../helpers/check.connect");
 
-const connectString = `mongodb://127.0.0.1:27017/shopDEV`;
-
+const connectString = `mongodb://${host}:${port}/${name}`;
+console.log(connectString);
 class Database {
   constructor() {
     this.connect();
