750ef92a07f7d27aeaff64a06b06524abf5d9b6a
fe-lam-dinh <dinhtronglam172000@gmail.com>
2023-04-24 14:48:35 +0700

    Section 6: Custom Dynamic Middleware for ApiKey and Permissions

---
+++ src/auth/checkAuth.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const { findId } = require("../services/apiKey.service");
+
+const HEADER = {
+  API_KEY: "x-api-key",
+  AUTHORIZATION: "authorization",
+};
+
+const apiKey = async (req, res, next) => {
+  try {
+    const key = req.headers[HEADER.API_KEY]?.toString();
+    if (!key) {
+      return res.status(403).json({
+        message: "Forbidden Error",
+      });
+    }
+    // check objKey
+    const objKey = await findId(key);
+    if (!objKey) {
+      return res.status(403).json({
+        message: "Forbidden Error",
+      });
+    }
+    req.objKey = objKey;
+    return next();
+  } catch (error) {}
+};
+
+const permission = (permission) => {
+  return (req, res, next) => {
+    if (!req.objKey.permissions) {
+      return res.status(403).json({
+        message: "permission dined",
+      });
+    }
+    console.log(`permissions:::`, req.objKey.permissions);
+    const validPermission = req.objKey.permissions.includes(permission);
+    if (!validPermission) {
+      return res.status(403).json({
+        message: "permission dined",
+      });
+    }
+    return next();
+  };
+};
+
+module.exports = {
+  apiKey,
+  permission,
+};
