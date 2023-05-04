d47bcba747a6e48e6c32aaeb92c25f3133f5cdb7
fe-lam-dinh <dinhtronglam172000@gmail.com>
2023-05-04 15:49:45 +0700

    FEAT: Section 12: Schema Product Ecommerce Cách thiết kế hàng triệu sản phẩm và tối ưu hóa truy vấn NHANH

---
+++ src/models/product.model.js
@@ -0,0 +1,58 @@
+"use strict";
+
+const { model, Schema } = require("mongoose");
+
+const DOCUMENT_NAME = "Product";
+const COLLECTION_NAME = "Products";
+
+const productSchema = new Schema(
+  {
+    product_name: { type: String, require: true },
+    product_thumb: { type: String, require: true },
+    product_description: String,
+    product_price: { type: Number, require: true },
+    product_quantity: { type: Number, require: true },
+    product_type: {
+      type: String,
+      require: true,
+      enum: ["Electronics", "Clothing", "Furniture"],
+    },
+    product_shop: { type: Schema.Type.ObjectId, ref: "User" },
+    product_attributes: { type: Schema.Types.Mixed, require: true },
+  },
+  {
+    collection: COLLECTION_NAME,
+    timestamps: true,
+  }
+);
+
+// define the product type = clothing
+const clothingSchema = new Schema(
+  {
+    brand: { type: String, require: true },
+    size: String,
+    material: String,
+  },
+  {
+    collection: "clothes",
+    timestamps: true,
+  }
+);
+// define the product type = electronic
+const electronicSchema = new Schema(
+  {
+    manufacturer: { type: String, require: true },
+    model: String,
+    color: String,
+  },
+  {
+    collection: "electronics",
+    timestamps: true,
+  }
+);
+
+module.exports = {
+  product: model(DOCUMENT_NAME, productSchema),
+  clothing: model("Clothing", clothingSchema),
+  electronic: model("Electronic", electronicSchema),
+};
